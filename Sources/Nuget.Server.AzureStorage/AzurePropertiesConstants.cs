d1d88aa4c1baeb37f74fe443425aba7620d8443a
AndrewKLong <andrew@andrewlongconsulting.com>
2014-10-31 11:24:36 -0500

    removed useless comments and changed the class type

--- Sources/Nuget.Server.AzureStorage/AzurePropertiesConstants.cs
+++ Sources/Nuget.Server.AzureStorage/AzurePropertiesConstants.cs
@@ -3,46 +3,13 @@
     /// <summary>
     /// Constants for the Azure properties metadata
     /// </summary>
-    internal sealed class AzurePropertiesConstants
+    public static class AzurePropertiesConstants
     {
-        /// <summary>
-        /// The created
-        /// </summary>
         public const string Created = "Created";
-
-        /// <summary>
-        /// The latest modification date
-        /// </summary>
         public const string LatestModificationDate = "LastModified";
-
-        /// <summary>
-        /// The last uploaded version
-        /// </summary>
         public const string LastUploadedVersion = "LastVersion";
-
-        /// <summary>
-        /// The last accessed
-        /// </summary>
         public const string LastAccessed = "LastAccessed";
-
         public const string Package = "Package";
-
-        ///// <summary>
-        ///// Subset of Constants for Package Metadata.
-        ///// </summary>
-        //public sealed class Package
-        //{
-        //    /// <summary>
-        //    /// The identifier
-        //    /// </summary>
-        //    public const string Id = "Package.Id";
-
-        //    /// <summary>
-        //    /// The version
-        //    /// </summary>
-        //    public const string Version = "Package.Version";
-        //}
-
         public const string PackageId = "PackageId";
     }
 
